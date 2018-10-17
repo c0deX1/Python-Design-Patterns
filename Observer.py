12b3ad484c695c7f45fbfec4be5540716b57aeb9
c0deX1 <sizv1996@hotmail.com>
2018-10-17 12:05:05 +0700

    Finally add observer

--- Observer.py
+++ Observer.py
@@ -19,7 +19,7 @@ class Subject(abc.ABC):
         """Function for notification observers"""
 
 
-class Observer(abs.ABC):
+class Observer(abc.ABC):
     @abc.abstractmethod
     def __init__(self):
         """Abstract class"""
@@ -61,6 +61,14 @@ class WeatherData(Subject):
         self.__humidity = humidity
 
     @property
+    def observers(self):
+        return self.__observers
+
+    @observers.setter
+    def observers(self, observers):
+        self.__observers = observers
+
+    @property
     def pressure(self):
         return self.__pressure
 
@@ -75,7 +83,8 @@ class WeatherData(Subject):
         self.__observers.remove(observer)
 
     def notifyObservers(self):
-        """"""
+        for obs in self.__observers:
+            obs.update(self.temperature, self.humidity, self.pressure)
 
     def measurementsChanged(self):
         self.notifyObservers()
@@ -89,13 +98,22 @@ class WeatherData(Subject):
 
 class CurrentConditionDisplay(Observer):
 
-    def __init__(self, weatherData):
+    def __init__(self, name, weatherData):
+        self.__name = name
         self.__temperature = 0
         self.__humidity = 0
         self.__pressure = 0
 
         self.__weatherData = weatherData
-        weatherData.registerObserver(self)
+        self.weatherData.registerObserver(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
 
     @property
     def temperature(self):
@@ -120,12 +138,29 @@ class CurrentConditionDisplay(Observer):
     @pressure.setter
     def pressure(self, pressure):
         self.__pressure = pressure
+    @property
+    def weatherData(self):
+        return self.__weatherData
+
+    @weatherData.setter
+    def weatherData(self, weatherData):
+        self.__weatherData = weatherData
 
     def update(self, temp, humidity, pressure):
         self.__pressure = pressure
         self.__temperature = temp
         self.__humidity = humidity
+        self.display()
 
     def display(self):  # not implemented, should create interface or abc class
-        print("Current conditions: " + str(self.temperature) + " C degrees, " + str(
+        print("Current conditions by " + self.name + " : " + str(self.temperature) + " C degrees, " + str(
             self.humidity) + "% humidity and " + str(self.pressure) + " bar pressure")
+
+
+weatherData = WeatherData()
+conditionDisplay1 = CurrentConditionDisplay("Observer 1", weatherData)
+conditionDisplay2 = CurrentConditionDisplay("Observer 2", weatherData)
+
+weatherData.setMeasurements(80, 65, 30.4)
+weatherData.setMeasurements(82, 70, 29.2)
+weatherData.setMeasurements(78, 90, 29.2)
